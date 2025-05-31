bfd5a58e8cf2dfb4f11f3b22aaa7834af1dc33aa
Aditya Singh <adityasingh40675@gmail.com>
2025-05-31 06:56:02 +0530

    feat(server): add method to update and delete journals

--- next-app/app/api/journals/[id]/route.ts
+++ next-app/app/api/journals/[id]/route.ts
@@ -24,9 +24,10 @@ export async function GET(req: NextRequest, { params }: { params: { id: string }
         });
     }
 
+    const { id } = await params;
     const journal = await prisma.journal.findUnique({
         where: {
-            id: params.id,
+            id,
             userId: user.id,
         }
     });
@@ -51,6 +52,145 @@ export async function GET(req: NextRequest, { params }: { params: { id: string }
         status: HttpStatus.OK
     });
     
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({
+            success: false,
+            message: "Internal Server Error",
+            error
+        },{
+            status: HttpStatus.INTERNAL_SERVER_ERROR
+        });
+    }
+}
+
+
+export async function PUT(req: NextRequest, { params } : { params : { id: string }}) {
+    try {
+        const session = await auth.api.getSession({
+            headers: req.headers
+        });
+        const user = session?.user;
+
+        if (!user) {
+            return NextResponse.json({
+                success: false,
+                message: "Please login before accessing journal",
+                error: {
+                    message: "Unauthorized access"
+                }
+            },{
+                status: HttpStatus.UNAUTHORIZED
+            });
+        }
+
+        const { id } = await params;
+        const { content, title } = await req.json();
+        const userId = user.id;
+
+        let journal = await prisma.journal.findFirst({
+            where: {
+                id,
+                userId
+            }
+        })
+
+        if (!journal) {
+            return NextResponse.json({
+                success: false,
+                message: "Journal not found",
+                error: {
+                    message: "Invalid journal ID or access denied"
+                }
+            },{
+                status: HttpStatus.NOT_FOUND
+            });
+        }
+
+        journal = await prisma.journal.update({
+            where: {
+                id,
+                userId
+            },
+            data: {
+                content,
+                title
+            }
+        })
+
+        return NextResponse.json({
+            success: true,
+            message: "Journal updated successfully",
+            data: journal
+        }, {
+            status: HttpStatus.OK
+        })
+
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({
+            success: false,
+            message: "Internal Server Error",
+            error
+        },{
+            status: HttpStatus.INTERNAL_SERVER_ERROR
+        });
+    }
+}
+
+export async function DELETE(req: NextRequest, { params }: { params: { id: string }}) {
+    try {
+        const session = await auth.api.getSession({
+            headers: req.headers
+        });
+        const user = session?.user;
+
+        if (!user) {
+            return NextResponse.json({
+                success: false,
+                message: "Please login before accessing journal",
+                error: {
+                    message: "Unauthorized access"
+                }
+            },{
+                status: HttpStatus.UNAUTHORIZED
+            });
+        }
+
+        const userId = user.id;
+        const { id } = await params;
+        const journal = await prisma.journal.findFirst({
+            where: {
+                id,
+                userId
+            }
+        })
+
+        if(!journal) {
+            return NextResponse.json({
+                success: false,
+                message: "Journal with this id not found",
+                error: {
+                    message: "Journal doesn't exist"
+                }
+            })
+        }
+
+        await prisma.journal.delete({
+            where: {
+                id,
+                userId
+            }
+        })
+
+        return NextResponse.json({
+            success: true,
+            message: "Journal deleted successfully",
+            data: {}
+        }, {
+            status: HttpStatus.OK
+        })
+
     } catch (error) {
         console.error(error);
         return NextResponse.json({
